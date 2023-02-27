38cb9a17b334e2f26afa6e9e2def76a854cd88de
Michael Tenkorang Jr <82475246+tenkorangjr@users.noreply.github.com>
2023-02-26 23:14:27 -0500

    Passed tests

--- AgentTest.java
+++ AgentTest.java
@@ -17,6 +17,34 @@ public class AgentTest {
             SocialAgent socialAgent = new SocialAgent(50, 50, 50);
             AntiSocialAgent antiSocialAgent = new AntiSocialAgent(50, 70, 10);
 
+            // verify
+            System.out.println(socialAgent.getRadius() + " == 50");
+            System.out.println(socialAgent.getX() + " == 50.0");
+            System.out.println(socialAgent.getY() + " == 50.0");
+
+            System.out.println(antiSocialAgent.getRadius() + " == 10");
+            System.out.println(antiSocialAgent.getX() + " == 50.0");
+            System.out.println(antiSocialAgent.getY() + " == 70.0");
+
+            // test setRadius
+            socialAgent.setRadius(20);
+
+            // verify
+            System.out.println(socialAgent.getRadius() + " == 20");
+
+        }
+        {
+            // Setup
+            SocialAgent socialAgent = new SocialAgent(10, 50, 200);
+            SocialAgent socialAgent1 = new SocialAgent(20, 60, 50);
+            Landscape scape = new Landscape(500, 500);
+            scape.addAgent(socialAgent1);
+            scape.addAgent(socialAgent);
+            socialAgent.updateState(scape);
+
+            // Verify
+            System.out.println(socialAgent.getX() + " " + socialAgent.getY() + " != 10 50 (after 5 runs at least)");
+
         }
     }
 }
--- LandscapeTest.java
+++ LandscapeTest.java
@@ -40,14 +40,29 @@ public class LandscapeTest {
             // Test getNeighbors
             // setup
             Landscape scape = new Landscape(200, 500);
+            scape.addAgent(new SocialAgent(10, 50, 50));
+            scape.addAgent(new AntiSocialAgent(50, 50, 200));
+            scape.addAgent(new AntiSocialAgent(60, 60, 10));
 
             // verify
-            System.out.println(scape + " == 0");
-            System.out.println(scape.getHeight() + " == 500");
-            System.out.println(scape.getWidth() + " == 200");
+            System.out.println(scape.getNeighbors(10, 50, 50).size() + " == 1");
+            System.out.println(scape.getNeighbors(50, 50, 200).size() + " == 2");
+        }
+        {
+            // Test updateAgents
+            // setup
+            Landscape scape = new Landscape(200, 500);
+            Landscape tester = new Landscape(200, 500);
+            scape.addAgent(new SocialAgent(10, 50, 50));
+            tester.addAgent(new SocialAgent(10, 50, 50));
+            scape.addAgent(new AntiSocialAgent(50, 50, 200));
+            tester.addAgent(new AntiSocialAgent(50, 50, 200));
+            scape.addAgent(new AntiSocialAgent(60, 60, 10));
+            tester.addAgent(new AntiSocialAgent(60, 60, 10));
+            scape.updateAgents();
 
-            // test
-            assert scape != null : "Error in Landscape::Landscape()";
+            // verify
+            System.out.println(scape.equals(tester) + " == false");
         }
     }
 }
